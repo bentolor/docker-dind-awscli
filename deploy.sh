b4c27ef2b14bcfffbc4cd4ae17d8e6c2ad8ba08c
Sai Varun Reddy <saivarunvishal@gmail.com>
2022-04-07 18:33:31 +0530

    Adds deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+function deploy_image(){
+    cluster_name=$1
+    service_name=$2
+    NEW_IMAGE=$3
+    TASK_ARN=$(aws ecs describe-services --service $service_name --cluster $cluster_name | jq ".services[].taskDefinition" | sed -e 's/^"//' -e 's/"$//')
+    aws ecs describe-task-definition --task-definition $TASK_ARN>describe-task.json
+    sed -i -e "s|\"image\".*|\"image\":\"$NEW_IMAGE\"\,|g" describe-task.json
+    CONTAINER_DEFS=$(cat describe-task.json | jq ".taskDefinition.containerDefinitions")
+    echo "{\"containerDefinitions\":$CONTAINER_DEFS}" | cat>container-def.json
+    NEW_TASK_DEF_VERSION=$(aws ecs register-task-definition --family $TASK_DEF_FAMILY_NAME --cli-input-json file://container-def.json | jq ".taskDefinition.revision")
+    aws ecs update-service --cluster $CLUSTER_NAME --service $SERVICE_NAME --task-definition "$TASK_DEF_FAMILY_NAME:$NEW_TASK_DEF_VERSION" --force-new-deployment
+}
