cf0e567ccf31917dda44e21221a1b83c6050812f
Benjamin Schmid <bentolor@users.noreply.github.com>
2023-07-27 05:53:32 +0200

    build(build.sh): include docker base image version in version tags

--- build.sh
+++ build.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
-sudo docker buildx build --no-cache --platform linux/amd64 -t bentolor/docker-dind-awscli:latest "."
-sudo docker buildx build -f Dockerfile.dind --no-cache --platform linux/amd64 -t bentolor/docker-dind-awscli:dind "."
+set -exu
+sudo docker buildx build --platform linux/amd64 -t bentolor/docker-dind-awscli:latest "."
+sudo docker buildx build -f Dockerfile.dind --platform linux/amd64 -t bentolor/docker-dind-awscli:dind "."
 
 FULLVER=$(sudo docker run -t --rm  bentolor/docker-dind-awscli aws --version)
-echo "Full version: $FULLVER"
+#echo "AWS CLI complete versionstring: $FULLVER"
 VER=$(echo $FULLVER | cut -f 1 -d ' ' | cut -f 2 -d '/')
-echo "Detected AWS CLI Version: $VER"
+#echo "Extracted AWS CLI Version: $VER"
+DOCKERFULLVER=$(sudo docker run -t --rm  bentolor/docker-dind-awscli docker --version)
+#echo "Docker CLI complete versionstring: $VER"
+DOCKERVER=$(echo $DOCKERFULLVER | cut -f 3 -d ' ' | cut -f 1 -d ',')
+#echo "Extracted Docker CLI Version: $DOCKERVER"
 
-sudo docker tag bentolor/docker-dind-awscli:latest bentolor/docker-dind-awscli:$VER 
-sudo docker tag bentolor/docker-dind-awscli:dind bentolor/docker-dind-awscli:dind-$VER
+#echo "Tagging bentolor/docker-dind-awscli:$VER-$DOCKERVER"
+sudo docker tag bentolor/docker-dind-awscli:latest bentolor/docker-dind-awscli:$VER-docker-$DOCKERVER
+#echo "Tagging bentolor/docker-dind-awscli:dind-$VER-$DOCKERVER"
+sudo docker tag bentolor/docker-dind-awscli:dind bentolor/docker-dind-awscli:$VER-dind-$DOCKERVER
 
-echo "Pushing..."
-sudo docker push bentolor/docker-dind-awscli:$VER 
-sudo docker push bentolor/docker-dind-awscli:dind-$VER
+sudo docker push bentolor/docker-dind-awscli:$VER-docker-$DOCKERVER
+sudo docker push bentolor/docker-dind-awscli::$VER-dind-$DOCKERVER
 sudo docker push bentolor/docker-dind-awscli:latest
 sudo docker push bentolor/docker-dind-awscli:dind
